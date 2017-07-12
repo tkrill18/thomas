1c4d8d95f25dc1849869733ea069c888fecf9803
Thomas_ASC <tkrill18@gmail.com>
2017-07-12 09:29:44 -0400

    Updated with rand() function

--- week0/thomas.js
+++ week0/thomas.js
@@ -1,1 +1,16 @@
-console.log("Thomas is awesome!");
+/*  
+    TASK
+    Create rand() functionTakes a number argument
+    Takes a number argument
+    Returns a random number between 0 and that number
+
+    PSEUDOCODE
+    multiplly Math.random() by number given
+    Remove decimal part
+    return that remaining integer
+*/
+
+function rand(num) {
+    // returns a random number between 0 and num
+    return Math.floor(Math.random() * num);
+}
