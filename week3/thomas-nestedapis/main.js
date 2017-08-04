2491425b22a66a772ba2d2150272ad8bf26f2b92
Thomas Krill <tkrill18@gmail.com>
2017-08-04 12:01:34 -0400

    OAuth Example complete

--- week3/thomas-nestedapis/main.js
+++ week3/thomas-nestedapis/main.js
@@ -1,16 +1,16 @@
 // Defines global variables for later use.
 var randomUserURL = "https://randomuser.me/api/";
 var restCountriesURL = "https://restcountries.eu/rest/v2/"
-
 var availableCodes = ["AU", "BR", "CA", "CH", "DE", "DK", "ES", "FI", "FR", "GB", "IE", "IR", "NL", "NZ", "TR", "US"];
 
-// Makes an AJAX call to the RU API.
+// Makes an initial AJAX call to the Random User API.
 $.ajax({
     url:randomUserURL
 }).done(function(data){
     handleRUData(data);
 });
 
+// Defines the callback function for the Random User AJAX call.
 var handleRUData = (data) => {
     // Parses the JSON for relevant information.
     var firstName = data.results[0].name.first;
@@ -21,7 +21,7 @@ var handleRUData = (data) => {
 
     var age = 2017 - data.results[0].dob.split("-")[0];
 
-    // Appends the RU information to the DOM.
+    // Updates the DOM with the Random User information.
     $("#card").html(
         "<h1>" +
             firstName + " " + lastName + ", " + age +
@@ -34,7 +34,7 @@ var handleRUData = (data) => {
         "' >"
     );
 
-    // Makes a call to the RC API.
+    // Makes an AJAX call to the REST Countries API.
     $.ajax({
         url:restCountriesURL + "alpha/?codes=" + countryCode
     }).done(function(data){
@@ -43,43 +43,54 @@ var handleRUData = (data) => {
 
 }
 
+// Defines the callback function for the REST Countries AJAX call.
 var handleRCData = (data) => {
+    // Parses the JSON for relevant information.
     var countryName = data[0].name;
     var currentString = $("#location").html();
+
+    // Updates the DOM with the REST Countries information.
     $("#location").html(currentString + ", " + countryName);
 }
 
-// Makes an AJAX call to the RC API for country names.
+// Makes an AJAX call to the RC API for country names to fill out the select form.
 $.ajax({
     url:restCountriesURL + "all"
 }).done(function(data) {
     loadRCDropdown(data);
 })
 
+// Defines the callback function for the REST Countries AJAX call.
 var loadRCDropdown = (data) => {
+    // Loops through all the countries in the JSON.
     for (country of data) {
-        //console.log(country.name);
+        // Adds a country name to the select form if it is on the list of available country codes.
+        // `myArray.indexOf(myElement)` returns `-1` if `myElement` is not in `myArray`.
         if (availableCodes.indexOf(country.alpha2Code) != -1) {
             $("#mySelect").append(
+                // Sets the `value` attribute of the `option` to the respective country code.
                 "<option value=" + country.alpha2Code + ">"+ country.name + "</option>"
             )
         }
     }
+    // Changes display from 'loading' to 'complete'.
     $("#myLabel").html("<p>Choose a country to meet somebody from&hellip;</p>")
     $("#mySelect").prop("disabled", false);
 }
 
-// Document Ready
+// Becomes available once the DOM is ready.
 $(function() {
+    // Updates the information card with a person from the selected country.
     $("#mySelect").on('change', function() {
+        // Obtains the country code stored in the `value` attribute.
         var countryCode = $(this).val();
+        // Makes an AJAX call to the Random User API with the selected country.
         $.ajax({
             url:randomUserURL + "?nat=" + countryCode
         }).done(function(data){
             handleRUData(data);
         });
     });
-
 })
 
 
